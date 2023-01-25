fd84369cddfbec15cb346b6a54fe8d4abacf2b64
akshay120594 <110121426+akshay120594@users.noreply.github.com>
2023-01-25 14:28:25 +0530

    first commit

---
+++ src/main/java/com/assignment/javaspringbootassignment/JavaSpringbootAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.javaspringbootassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootAssignmentApplication.class, args);
+	}
+
+}
